b1d82bbfab2f1b8f9381f414df5a15a92e31d9db
d.mamnitsky <d.mamnitsky.stc@innopolis.ru>
2018-06-01 12:28:37 +0200

    deleted useful directories from develop branch. add packages from not merging branch feature-pn13

---
+++ src/main/java/ru/innopolis/stc9/pojo/GroupStructure.java
@@ -0,0 +1,36 @@
+package ru.innopolis.stc9.pojo;
+
+public class GroupStructure {
+
+  private long id;
+  private long studentItem;
+  private long groupItem;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getStudentItem() {
+    return studentItem;
+  }
+
+  public void setStudentItem(long studentItem) {
+    this.studentItem = studentItem;
+  }
+
+
+  public long getGroupItem() {
+    return groupItem;
+  }
+
+  public void setGroupItem(long groupItem) {
+    this.groupItem = groupItem;
+  }
+
+}
---
+++ src/main/java/ru/innopolis/stc9/pojo/User.java
@@ -0,0 +1,46 @@
+package ru.innopolis.stc9.pojo;
+
+public class User {
+
+  private long id;
+  private String login;
+  private String password;
+  private long personId;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+  public long getPersonId() {
+    return personId;
+  }
+
+  public void setPersonId(long personId) {
+    this.personId = personId;
+  }
+
+}
